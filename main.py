c6cdbed55374435ffa791ae787607a4eb9494d23
jukoo <funscript@outlook.fr>
2022-07-16 14:42:06 +0000

    Improved AsciCam by adding command line flags for dynamic parameters and addapt ChangefontSize only for win32

--- main.py
+++ main.py
@@ -12,9 +12,27 @@
 # License: MIT License                                                                      #
 # --------------------------------------- [ Enjoy ] --------------------------------------- #
 
-import cv2, os
-from PIL import Image
+import cv2, os,sys, argparse ,signal  
+from PIL import Image 
+from enum  import Enum 
+from collections  import  namedtuple   
 
+
+class  AsciCamDefaultPramaters(Enum) :  
+    camera     = 0 
+    mirrored   = 1
+    vertical   = 100 
+    fontsize   = 1 
+    set_screen = 0   
+    char_list = "".join([' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N'])
+    
+defparams_attributes , defparams_values = (
+        AsciCamDefaultPramaters.__members__.keys()  ,
+        [  p_value.value for  p_value in  AsciCamDefaultPramaters.__members__.values()] 
+        )  
+
+AsciCam = namedtuple("AsciCam" ,defparams_attributes ) 
+AsciCam = AsciCam(*defparams_values)  
 
 def setscreen(col, line):
     try:
@@ -22,10 +40,20 @@ def setscreen(col, line):
     except:
         print("Failed to set screen.")
 
+def  os_support (supported_osplatform)  : 
+    def fcall ( _fcallable )  : 
+        def _restrict  (*args , **kwargs ) :  
+            if sys.platform.__eq__(supported_osplatform)  : 
+                from ctypes import POINTER, WinDLL, Structure, sizeof, byref
+                from ctypes.wintypes import BOOL, SHORT, WCHAR, UINT, ULONG, DWORD, HANDLE
+                _fcallable(*args ,**kwargs) 
 
+        return  _restrict 
+    return fcall 
+
+
+@os_support("win32")  
 def changeFontSize(size=2):  # Changes the font size to *size* pixels (Kind of, but not really.)
-    from ctypes import POINTER, WinDLL, Structure, sizeof, byref
-    from ctypes.wintypes import BOOL, SHORT, WCHAR, UINT, ULONG, DWORD, HANDLE
 
     LF_FACESIZE, STD_OUTPUT_HANDLE = 32, -11
 
@@ -79,21 +107,31 @@ def print_large_block(text):
 
 
 def get_ascii_from_image(im):
-    global char_list, mirrored
     lines = []
-    x_range = range(im.size[0])[::-1] if mirrored else range(im.size[0])
+    x_range = range(im.size[0])[::-1] if AsciCam.mirrored else range(im.size[0])
     for y in range(im.size[1]):
         line = []
         for x in x_range:
             pix = sum(im.getpixel((x, y))) / 3
-            char_list_pos = int((len(char_list) - 1) * pix / 255)
-            line.append(char_list[char_list_pos])
+            char_list_pos = int((len(AsciCam.char_list) - 1) * pix / 255)
+            line.append(AsciCam.char_list[char_list_pos])
         lines.append(''.join(line))
-    return ('\n' + ("\n" if fontsize != 1 else "")).join(lines)
+    return ('\n' + ("\n" if AsciCam.fontsize != 1 else "")).join(lines)
 
 
 def get_video_frms(path, format="cv2"):
+    try  :  
+        path =  int(path) 
+    except ValueError :  
+        #may be is a file  
+        if  os.access(path ,os.F_OK | os.R_OK) :...  # ok  
+        else  :  
+            sys.stderr.write(f"Sorry  cannot open  {path} \nAborting...") 
+            signal.raise_signal(signal.SIGABRT) 
+             
+
     cap = cv2.VideoCapture(path, cv2.CAP_DSHOW)
+    cap.open(path) 
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
@@ -112,31 +150,47 @@ def cv2_to_PIL(frame):
 
 
 def print_ascii_from_im(im):
-    global horizontal, vertical, set_screen
-    horizontal = int(vertical * 2 * im.size[0] / im.size[1])
-    if set_screen == 0:
-        set_screen = 1
-        setscreen(horizontal, vertical * (2 if fontsize != 1 else 1))
-    im = im.resize((horizontal, vertical))
+    global AsciCam 
+    horizontal = int(AsciCam.vertical * 2 * im.size[0] / im.size[1])
+    if AsciCam.set_screen == 0:
+        AsciCam =  AsciCam._replace(set_screen = 1)  
+        setscreen(horizontal, AsciCam.vertical * (2 if AsciCam.fontsize != 1 else 1))
+    im = im.resize((horizontal, AsciCam.vertical))
     print_large_block(get_ascii_from_image(im) + '\n')
 
 
-def main():
-    [print_ascii_from_im(im) for im in get_video_frms(camera, 'PIL')]
+def main(): 
+    global AsciCam 
+    description = "Converts your webcam input to ascii and prints it on the screen."
+    stdargs  = argparse.ArgumentParser(description=description  ,prog="AsciCam")  
+    stdargs.add_argument("-c" ,"--camera"   , help="Select Camera  device  number or  file if your  are in unix system")
+    stdargs.add_argument("-m","--mirrored"  , type=int,  help="")  
+    stdargs.add_argument("-f" ,"--fontsize" , type=int ,help="Fontsize of Ascii charaters ") 
+    stdargs.add_argument("-v" ,"--vertical" , type=int, help="")  
+    stdargs.add_argument("-s" , "--set-screen" ,type=int ,help="")  
+    stdargs.add_argument("-V" ,"--version"  , action="store_true" , help=f"Show current  version of AsciCam ")  
+    
+    args  = stdargs.parse_args()    
+    if  args.version :  
+        print(f"{description}\nVersion of AsciCam   x.x.x ")  
+        sys.exit(0) 
+
+    camera   =  (AsciCam.camera   , args.camera  )[args.camera   is not None] 
+    mirrored =  (AsciCam.mirrored , args.mirrored)[args.mirrored is not None]   
+    fontsize =  (AsciCam.fontsize , args.fontsize)[args.fontsize is not None] 
+    vertical =  (AsciCam.vertical , args.vertical)[args.vertical is not None] 
+    set_screen= (AsciCam.set_screen,args.set_screen)[args.set_screen is not None] 
+
+    AsciCam = AsciCam._replace(camera=camera, mirrored=mirrored  , fontsize=fontsize  , vertical=vertical, set_screen=set_screen) 
+    
+    [print_ascii_from_im(im) for im in get_video_frms(AsciCam.camera, 'PIL')]
 
 
 if __name__ == '__main__':
-    camera = 0
-    mirrored = 1
-    vertical = 100
-    fontsize = 1  # 3 is buggy, so don't use 3.
-
-    set_screen = 0     
-    char_list = "".join([' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N'])
-
     try:
-        changeFontSize(size=fontsize)
-    except:
+        changeFontSize(size=AsciCam.fontsize)
+    except Exception as  Xcption : 
+        print(f"{Xcption}") 
         print("Failed to set font size.")
 
     main()
